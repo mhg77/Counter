e8f39514616b5e2ffc39edcc7d57bbe738b61113
Mikhail Asailov <hashier@yandex.ru>
2023-03-26 20:12:22 +0300

    Исправлены модификаторы доступа, добавлен текстовой вывод при попытке уменьшить счетчик меньше 0

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private var userCount: Int = 0
     
-    @IBOutlet weak var countButtonMinus: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var historyTextView: UITextView!
-    @IBOutlet weak var countButtonPlus: UIButton!
-    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet private weak var countButtonMinus: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
+    @IBOutlet private weak var historyTextView: UITextView!
+    @IBOutlet private weak var countButtonPlus: UIButton!
+    @IBOutlet private weak var countLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,42 +23,43 @@ class ViewController: UIViewController {
         historyTextView.isEditable = false
     }
     
-    func currentDate() -> String {
+    private func currentDate() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH.mm.ss"
         return dateFormatter.string(from: Date())
     }
     
-    func appendHistory(message: String) {
-            let historyMessage = "\n\(currentDate()): \(message)\n"
-            historyTextView.text.append(historyMessage)
-            historyTextView.scrollRangeToVisible(NSRange(location: historyTextView.text.count, length: 0))
-        }
-
-        @IBAction func buttonDidTapPlus(_ sender: Any) {
-            userCount += 1
+    private func appendHistory(message: String) {
+        let historyMessage = "\n\(currentDate()): \(message)\n"
+        historyTextView.text.append(historyMessage)
+        historyTextView.scrollRangeToVisible(NSRange(location: historyTextView.text.count, length: 0))
+    }
+    
+    @IBAction private func buttonDidTapPlus(_ sender: Any) {
+        userCount += 1
+        countLabel.text = "\(userCount)"
+        appendHistory(message: "значение изменено на +1")
+    }
+    
+    @IBAction private func buttonDidTapMinus(_ sender: Any) {
+        if userCount == 0 {
             countLabel.text = "\(userCount)"
-            appendHistory(message: "значение изменено на +1")
+            appendHistory(message: "попытка уменьшить значение счётчика ниже 0")
+        } else {
+            userCount -= 1
+            countLabel.text = "\(userCount)"
+            appendHistory(message: "значение изменено на -1")
         }
-        
-        @IBAction func buttonDidTapMinus(_ sender: Any) {
-            if userCount == 0 {
-                return
-            } else {
-                userCount -= 1
-                countLabel.text = "\(userCount)"
-                appendHistory(message: "значение изменено на -1")
-            }
+    }
+    
+    @IBAction private func buttonDidTapReset(_ sender: Any) {
+        if userCount == 0 {
+            return
+        } else {
+            userCount = 0
+            countLabel.text = "0"
+            appendHistory(message: "значение сброшено")
         }
-        
-        @IBAction func buttonDidTapReset(_ sender: Any) {
-            if userCount == 0 {
-                return
-            } else {
-                userCount = 0
-                countLabel.text = "0"
-                appendHistory(message: "значение сброшено")
-            }
     }
 }
 
